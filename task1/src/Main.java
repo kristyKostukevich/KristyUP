d9f0f76a851608821b50a50503d861ef9fc3e2e6
Kristy <kostukevichkristy@gmail.com>
2017-02-10 19:18:46 +0300

    add status

---
+++ task1/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+/**
+ * Created by Kristy on 20.12.2016.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+      ExpTailor one = new ExpTailor();
+        one.printX();
+        one.couting();
+        one.bigDecimalCounting();
+
+    }
+}
