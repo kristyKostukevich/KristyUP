dbc0dba0a3cfc1c426a776aa0b2f847929dd34ed
Kristy <kostukevichkristy@gmail.com>
2017-04-03 22:05:32 +0300

    delete JS and UI

--- JS/saver.js
+++
@@ -1,8 +0,0 @@
-
-window.addEventListener('beforeunload', function () {
-    localStorage.setItem('content', JSON.stringify(articlesService.articles));
-    localStorage.setItem('tags', JSON.stringify(articlesService.arraytags));
-    if (articlesService.user) localStorage.setItem('user', articlesService.user);
-    else localStorage.removeItem('user');
-
-});
