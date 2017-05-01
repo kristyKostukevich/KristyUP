7f844e793b8054d30ffd35e09fb96b2aada39f9f
Kristy <kostukevichkristy@gmail.com>
2017-05-01 19:03:19 +0300

    added passport

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let express = require('express');
+
 let app = express();
+
 let bodyParser = require('body-parser');
 let articleMapper = require(__dirname+'/server/db/data-mapper/article-mapper');
 let tagsMapper = require(__dirname+'/server/db/data-mapper/tags-mapper');
@@ -9,9 +11,9 @@ app.use(express.static('public'));
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 
 app.get('/articles', (req, res) => {
@@ -61,3 +63,55 @@ app.delete('/user',  (req, res)=> {
     userMapper.deleteUser();
     res.end;
 });
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let cookieParser = require('cookie-parser');
+let passport = require('passport');
+let expressSession = require('express-session');
+
+// let LocalStrategy = require('passport-local').Strategy;
+
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+let LocalStrategy   = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy({
+        passReqToCallback : true
+    },
+    function(req, username, password, done) {
+        let user = userMapper.getUserByName(username)[0];
+        if (!user) {
+            console.log('User Not Found with username '+username);
+            return done(null, false);
+        }
+        if (user.password != password) {
+            console.log('Invalid Password');
+            return done(null, false);
+        }
+        return done(null, user);
+    }));
+
+
+
+
+app.post('/login', passport.authenticate('local'), (req, res) => {
+    res.sendStatus(200);
+});
+
+
+
+
+
